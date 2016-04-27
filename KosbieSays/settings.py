bf505f9456d0dadab8c57d8020f5161d4f0aa2cb
rnvarma <rohan@prompthire.com>
2016-04-27 09:48:39 -0400

    fixed'

--- KosbieSays/settings.py
+++ KosbieSays/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(MAIN_DIR, 'static'),
 )
 
 STATIC_URL = '/static/'
